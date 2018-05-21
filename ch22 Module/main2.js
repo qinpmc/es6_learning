0a4ba94d72e4703d71229e8f8c63326864bae5c7
qinpmc <qinpmc@163.com>
2018-05-21 23:21:54 +0800

    modify main2 demo

--- ch22 Module/main2.js
+++ ch22 Module/main2.js
@@ -1,3 +1,6 @@
-import {m} from "./profile2_1.js"
+import { m } from "./profile2_1.js"
+console.log(m); //999 
 
-console.log(m); //999
+/*
+import * as p2 from "./profile2_1.js"
+console.log(p2.m); //999*/
--- ch22 Module/profile2_1.js
+++ ch22 Module/profile2_1.js
@@ -1,2 +1,9 @@
 var n = 999;
+export function area(radius) {
+    return Math.PI * radius * radius;
+}
+
+export function circumference(radius) {
+    return 2 * Math.PI * radius;
+}
 export{n as m};
