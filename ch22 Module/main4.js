6b2f3356bdd36f31a1c24ff16e1ed3ed07b4fb99
qinpmc <850823973@qq.com>
2019-02-27 10:09:17 +0800

    add module

--- ch22 Module/main4.js
+++ ch22 Module/main4.js
@@ -1,11 +1,16 @@
 import _,{ each,forEach} from "./profile4_exportDefault.js";
 
-_("this is default");
+_("this is default");// this is default
 
-each([3,4,5]);
-forEach([-9,-3,-4])
+each([3,4,5]); // 3,4,5
+forEach([-9,-3,-4]); //-9,-3,-4
 
+
+
+/*import varA from "./profile4_exportDefault.js";
+console.log(varA);*/
 
 /*
-import varA from "./profile4_exportDefault.js";
-console.log(varA);*/
+ƒ (msg){
+    console.log(msg);
+}*/
